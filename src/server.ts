e27c3485c6062a6a7ca5baff79f0afc5b04e8781
Evandro oliveira <vandoaparecido@hotmail.com>
2021-02-22 22:24:24 -0300

    Criando primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req,res)=>{
+    res.json({Hello: "NLW!"})
+})
+
+app.post('/create', (req,res)=>{
+   return res.json({Nome: "Evandro Oliveira", Email: "vandoaparecido@hotmail.com"})
+})
+
 app.listen(3333, () => console.log("Servidor rodando"));
